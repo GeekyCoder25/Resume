12f49b7d2642254a3ac55136e342bfe43346fb4d
Geeky Coder <toyibe25@gmail.com>
2022-07-28 23:19:01 +0100

    added updates

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/resume" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
